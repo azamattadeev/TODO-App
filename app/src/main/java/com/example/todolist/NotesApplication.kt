e978cce5ee775f139d53c1014565cbd557662777
atadeev <Azamat.Tadeev@telekom.com>
2021-03-17 14:50:24 +0300

    - Create NotesApplication.kt  - Add NoteListActivity.kt

--- app/src/main/java/com/example/todolist/NotesApplication.kt
+++ app/src/main/java/com/example/todolist/NotesApplication.kt
@@ -5,6 +5,10 @@ import com.example.todolist.dagger.AppComponent
 import com.example.todolist.dagger.DaggerAppComponent
 import com.example.todolist.dagger.DatabaseModule
 import com.example.todolist.dagger.AppContextModule
+import com.example.todolist.persistence.Note
+import com.example.todolist.persistence.NoteDao
+import java.util.concurrent.Executors
+import javax.inject.Inject
 
 class NotesApplication: Application() {
 
@@ -14,10 +18,14 @@ class NotesApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-         appComponent = DaggerAppComponent.builder()
-             .appContextModule(AppContextModule(this))
-             .databaseModule(DatabaseModule())
-             .build()
+        buildAppComponent()
+    }
+
+    private fun buildAppComponent() {
+        appComponent = DaggerAppComponent.builder()
+                .appContextModule(AppContextModule(this))
+                .databaseModule(DatabaseModule())
+                .build()
     }
 
 }
---
+++ app/src/main/java/com/example/todolist/activities/NoteListActivity.kt
@@ -0,0 +1,53 @@
+package com.example.todolist.activities
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.NotesApplication
+import com.example.todolist.R
+import com.example.todolist.mvp.presenter.NoteListPresenter
+import com.example.todolist.mvp.view.NoteListView
+import com.example.todolist.persistence.Note
+import com.example.todolist.recycler.NotesAdapter
+import javax.inject.Inject
+
+class NoteListActivity: AppCompatActivity(), NoteListView {
+
+    @Inject lateinit var presenter: NoteListPresenter
+
+    private lateinit var notesAdapter: NotesAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notes_list)
+        NotesApplication.appComponent.inject(this)
+
+        presenter.handler = Handler(Looper.getMainLooper())
+        presenter.attachView(this)
+
+        val recycler = findViewById<RecyclerView>(R.id.activity_notes_list__notes_recycler)
+        recycler.layoutManager = LinearLayoutManager(this)
+
+        notesAdapter = NotesAdapter(emptyList())
+        recycler.adapter = notesAdapter
+
+        presenter.loadNoteList()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.detachView()
+    }
+
+    override fun navigateToNoteEditor() {
+        TODO()
+    }
+
+    override fun onAllNotesLoaded(notes: List<Note>) {
+        notesAdapter.items = notes
+    }
+
+}
